012ee4ffe5f60d21ce16fae1c7dd27e34c3529a9
marcusw <marcusw@pageuppeople.com>
2017-01-10 13:58:07 +1100

    Add missing file

---
+++ plugins/rail-history-cms/index.php
@@ -0,0 +1,31 @@
+<?php 
+$pageTitle = "Site Management";
+include_once("common/dbConnection.php");
+include_once("common/header.php");
+?>
+<table class="centeredTable">
+<tr><td>
+<h4>Location Types:</h4>
+<a href="enterNewLocationTypes.php">Enter New Location Types</a><br>
+<a href="listLocationTypes.php">List All Location Types</a><br>
+
+</td>
+</tr>
+
+<tr><td>
+<h4>Safeworking Types:</h4>
+<a href="enterNewSafeworkingTypes.php">Enter New Safeworking Type</a><br>
+<a href="listSafeworkingTypes.php">List All Safeworking Types</a><br>
+</td>
+</tr>
+
+<tr><td>
+<h4>Location images:</h4>
+<a href="listLocationImages.php?type=images">Locations with images</a><br>
+<a href="listLocationImages.php?type=required">Locations without images</a><br>
+<a href="listLocationImages.php">All locations</a>
+
+</td></tr>
+</table>
+
+<?php include_once("common/footer.php"); ?>
