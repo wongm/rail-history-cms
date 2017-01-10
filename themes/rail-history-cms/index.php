0e1bd42921ff702d0dcd5ec74a42812b1386bff7
marcusw <marcusw@pageuppeople.com>
2017-01-10 13:20:33 +1100

    Ensure gallery with no images does not crash home page

--- themes/rail-history-cms/index.php
+++ themes/rail-history-cms/index.php
@@ -71,7 +71,7 @@ drawUpdatedPagesTable($updates['result'], true);
 
 setCustomPhotostream("", "i.albumid, DATE(i.date)", "i.date DESC");
 
-for ($albumCount = 1; $albumCount < 7; $albumCount++) {
+for ($albumCount = 1; $albumCount < 7 && $albumCount < getNumPhotostreamImages(); $albumCount++) {
 	next_photostream_image();
 	if (($albumCount % 3) == 1)
 	{
