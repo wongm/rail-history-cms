1a9383b641b21612e3814597d5b0fd952d8b051d
marcusw <marcusw@pageuppeople.com>
2017-01-24 09:50:32 +1100

    Enhance custom cacheManager plugin - remove need to use standard plugin, that has the dangerous 'clear cached imges' button

--- plugins/rail-history-cms-cache-manager.php
+++ plugins/rail-history-cms-cache-manager.php
@@ -26,21 +26,75 @@ zp_register_filter('admin_utilities_buttons', 'cacheManager_railHistoryCMS::over
  */
 class cacheManager_railHistoryCMS {
 
-	static function overviewbutton($buttons) {
-    	
+	static function overviewbutton($buttons) {		
+
 		$buttons[] = array(
-									'category'=>gettext('Cache'),
-									'enable'=>true,
-									'button_text'=>gettext('Purge CMS cache'),
-									'formname'=>'clearcache_button',
-									'action'=>WEBPATH . "/" . USER_PLUGIN_FOLDER.'/rail-history-cms-cache-manager.php?action=clear_railHistoryCMS_cache',
-									'icon'=>'images/edit-delete.png',
-									'title'=>gettext('Clear the static Rail History CMS cache. HTML pages will be re-cached as they are viewed.'),
-									'alt'=>'',
-									'hidden'=> '<input type="hidden" name="action" value="clear_railHistoryCMS_cache">',
-									'rights'=> ADMIN_RIGHTS,
-									'XSRFTag'=>'clear_railHistoryCMS_cache'
+						'category'		 => gettext('Cache'),
+						'enable'			 => $enable,
+						'button_text'	 => gettext('Cache manager'),
+						'formname'		 => 'cacheManager_button',
+						'action'			 => WEBPATH . '/' . ZENFOLDER . '/' . PLUGIN_FOLDER . '/cacheManager/cacheImages.php?page=overview&tab=images',
+						'icon'				 => 'images/cache.png',
+						'alt'					 => '',
+						'hidden'			 => '',
+						'rights'			 => ADMIN_RIGHTS,
+						'title'				 => $title
+		);
+		$buttons[] = array(
+						'category'=>gettext('Cache'),
+						'enable'=>true,
+						'button_text'=>gettext('Purge CMS cache'),
+						'formname'=>'clearcache_button',
+						'action'=>WEBPATH . "/" . USER_PLUGIN_FOLDER.'/rail-history-cms-cache-manager.php?action=clear_railHistoryCMS_cache',
+						'icon'=>'images/edit-delete.png',
+						'title'=>gettext('Clear the static Rail History CMS cache. HTML pages will be re-cached as they are viewed.'),
+						'alt'=>'',
+						'hidden'=> '<input type="hidden" name="action" value="clear_railHistoryCMS_cache">',
+						'rights'=> ADMIN_RIGHTS,
+						'XSRFTag'=>'clear_railHistoryCMS_cache'
 									);
+		if (class_exists('RSS')) {
+			$buttons[] = array(
+							'XSRFTag'			 => 'clear_cache',
+							'category'		 => gettext('Cache'),
+							'enable'			 => true,
+							'button_text'	 => gettext('Purge RSS cache'),
+							'formname'		 => 'purge_rss_cache.php',
+							'action'			 => WEBPATH . '/' . ZENFOLDER . '/admin.php?action=clear_rss_cache',
+							'icon'				 => 'images/edit-delete.png',
+							'alt'					 => '',
+							'title'				 => gettext('Delete all files from the RSS cache'),
+							'hidden'			 => '<input type="hidden" name="action" value="clear_rss_cache" />',
+							'rights'			 => ADMIN_RIGHTS
+			);
+		}
+		$buttons[] = array(
+						'category'		 => gettext('Cache'),
+						'enable'			 => true,
+						'button_text'	 => gettext('Purge HTML cache'),
+						'formname'		 => 'clearcache_button',
+						'action'			 => WEBPATH . '/' . ZENFOLDER . '/admin.php?action=clear_html_cache',
+						'icon'				 => 'images/edit-delete.png',
+						'title'				 => gettext('Clear the static HTML cache. HTML pages will be re-cached as they are viewed.'),
+						'alt'					 => '',
+						'hidden'			 => '<input type="hidden" name="action" value="clear_html_cache">',
+						'rights'			 => ADMIN_RIGHTS,
+						'XSRFTag'			 => 'ClearHTMLCache'
+		);
+		
+		$buttons[] = array(
+						'category'		 => gettext('Cache'),
+						'enable'			 => true,
+						'button_text'	 => gettext('Purge search cache'),
+						'formname'		 => 'clearcache_button',
+						'action'			 => WEBPATH . '/' . ZENFOLDER . '/admin.php?action=clear_search_cache',
+						'icon'				 => 'images/edit-delete.png',
+						'title'				 => gettext('Clear the static search cache.'),
+						'alt'					 => '',
+						'hidden'			 => '<input type="hidden" name="action" value="clear_search_cache">',
+						'rights'			 => ADMIN_RIGHTS,
+						'XSRFTag'			 => 'ClearSearchCache'
+		);
 		return $buttons;
 	}
 }
