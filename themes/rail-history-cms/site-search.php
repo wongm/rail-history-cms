7ee9b20c2a41cd26bc8dd5d2e3bfc1d6f85f3059
Marcus Wong <marcusthomaswong@gmail.com>
2017-09-12 08:24:32 +1000

    Update Google Site Search

--- themes/rail-history-cms/site-search.php
+++ themes/rail-history-cms/site-search.php
@@ -10,10 +10,18 @@ include_once("common/header.php");
 <?php include_once("common/midbar.php"); ?>
 <h3><?php echo $pageTitle; ?></h3>
 <p>You are currently searching this site via Google: you can also <a href="<?php echo SEARCH_URL_PATH ?>?words=<?php echo $_GET['q']?>">search the Gallery</a>.</p>
-<script src="http://www.google.com/jsapi" type="text/javascript"></script>
-<script type="text/javascript" src="<?php echo $_zp_themeroot ?>/js/googlesearch.js"></script>
-<div id="results">Loading...</div>
-<input style="display:none" id="hidden-input" />
+<script>
+  (function() {
+    var cx = '013092749367948215647:livsqef13p8';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
 <?php
 include_once("common/footer.php"); 
 ?>
